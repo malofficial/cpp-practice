27ddceab5135f988cd1ff5a138aca060ac1344ac
Rahul Bagga <personalrahul@gmail.com>
2019-06-07 00:05:59 +0800

    direct initialization vs indirect initialization

--- initializer_list.cpp
+++ initializer_list.cpp
@@ -30,6 +30,21 @@ public:
     int size() const { return v.size(); }
 };
 
+struct S { int a, b; };
+struct SS { double a, b; };
+void f(S); // f() takes an S
+void g(S);
+void g(SS); // g() is overloaded
+void h()
+{
+f({1,2});   // OK: call f(S{1,2})
+//g({1,2});   // error : ambiguous
+g(S{1,2});  // OK: call g(S)
+g(SS{1,2}); // OK: call g(SS)
+
+}
+
+
 int main(int argc, char* argv[]) {
     std::cout << "Hello World" << std::endl;
 
